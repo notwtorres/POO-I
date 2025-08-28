8809c86adf12e47099651b9ce91ffecf7c59eef9
notwtorres <willy.dance.2006@gmail.com>
2025-08-28 16:58:53 -0600

    e

--- src/Run/Main.java
+++ src/Run/Main.java
@@ -1,12 +1,16 @@
 package Run;
 
+import Run2.CuentaBancaria;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        greetings();
+        Coche coche1 = new Coche("Toyota", "Corolla", 2020);
+        coche1.mostrarInfo();
+
+        Libro libro1 = new Libro("Toyota", "Corolla", 2020);
+        libro1.mostrarInfo();
     }
-    public static void greetings(){
-        System.out.println("Hello and welcome to Java programming!");
-    }
+
 }
